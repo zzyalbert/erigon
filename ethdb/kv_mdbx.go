d3ea40ea20102ce57b91acb8de30b4be147093a1
alex.sharov <AskAlexSharov@gmail.com>
2021-03-21 12:24:27 +0700

    better error messages

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -132,7 +132,7 @@ func (opts MdbxOpts) Open() (KV, error) {
 			return nil, err
 		}
 
-		if err = env.SetOption(mdbx.OptRpAugmentLimit, 32*1024*1024); err != nil {
+		if err = env.SetOption(mdbx.OptRpAugmentLimit, 8*1024*1024); err != nil {
 			return nil, err
 		}
 
