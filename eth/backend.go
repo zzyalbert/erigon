f468b188f207d2ba15c6cab7cba49a47e3682a25
alex.sharov <AskAlexSharov@gmail.com>
2021-04-11 07:51:09 +0700

    clean

--- eth/backend.go
+++ eth/backend.go
@@ -160,10 +160,10 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	//	return nil
 	//})
 	for i := 0; i < 100; i++ {
+		t := time.Now()
 		_ = chainDb.Walk(dbutils.Senders2, nil, 0, func(k, v []byte) (bool, error) {
 			return true, nil
 		})
-		t := time.Now()
 		for blockNum := uint64(7_000_000); blockNum <= 12_000_000; blockNum++ {
 			blockHash, _ := rawdb.ReadCanonicalHash(chainDb, blockNum)
 			_, _ = rawdb.ReadSenders(chainDb, blockHash, blockNum)
