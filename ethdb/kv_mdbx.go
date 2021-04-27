a4ba210a35ef17a0e4d8aede4bf7fba7709f37d6
alex.sharov <AskAlexSharov@gmail.com>
2021-04-27 10:02:22 +0700

    commit every 5 min

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -145,13 +145,13 @@ func (opts MdbxOpts) Open() (RwKV, error) {
 		if err = env.SetOption(mdbx.OptSpillMinDenominator, 8); err != nil {
 			return nil, err
 		}
-		if err = env.SetOption(mdbx.OptTxnDpInitial, 4*1024); err != nil {
+		if err = env.SetOption(mdbx.OptTxnDpInitial, 64*1024); err != nil {
 			return nil, err
 		}
-		if err = env.SetOption(mdbx.OptDpReverseLimit, 4*1024); err != nil {
+		if err = env.SetOption(mdbx.OptDpReverseLimit, 64*1024); err != nil {
 			return nil, err
 		}
-		if err = env.SetOption(mdbx.OptTxnDpLimit, opts.dirtyListMaxPages); err != nil {
+		if err = env.SetOption(mdbx.OptTxnDpLimit, 1024*1024); err != nil {
 			return nil, err
 		}
 	}
