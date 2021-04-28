2c32029e499f5d3d7e4f4d99b7b347dd2d64b69c
alex.sharov <AskAlexSharov@gmail.com>
2021-04-28 17:06:26 +0700

    run

--- ethdb/mdbx/mdbx.go
+++ ethdb/mdbx/mdbx.go
@@ -140,7 +140,7 @@ package mdbx
 //#include "mdbx.h"
 //#include "mdbxgo.h"
 //
-#cgo CFLAGS: -O2 -g -Wno-deprecated-declarations -pthread -W -Wall -Werror -Wextra -Wpedantic -fPIC -fvisibility=hidden -std=gnu11 -pthread -Wno-error=attributes -Wno-implicit-fallthrough -Wno-unused-function -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers
+#cgo CFLAGS: -O0 -g -Wno-deprecated-declarations -pthread -W -Wall -Werror -Wextra -Wpedantic -fPIC -fvisibility=hidden -std=gnu11 -pthread -Wno-error=attributes -Wno-implicit-fallthrough -Wno-unused-function -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers
 #cgo LDFLAGS: ${SRCDIR}/dist/mdbx-static.o
 */
 import "C"
