20e3349ccbf9c8c53e647e73320c2d2ec8044e89
alex.sharov <AskAlexSharov@gmail.com>
2021-04-28 14:22:33 +0700

    run

--- ethdb/kv_lmdb.go
+++ ethdb/kv_lmdb.go
@@ -591,7 +591,7 @@ func (tx *lmdbTx) Commit() error {
 	tx.closeCursors()
 
 	commitTimer := time.Now()
-	defer dbCommitBigBatchTimer.UpdateSince(commitTimer)
+	//defer dbCommitBigBatchTimer.UpdateSince(commitTimer)
 
 	if err := tx.tx.Commit(); err != nil {
 		return err
