9518f955ffc5994512449b1914a906c8d179ef73
alex.sharov <AskAlexSharov@gmail.com>
2021-04-29 09:44:10 +0700

    txn full fix - cursors leak

--- ethdb/kv_lmdb.go
+++ ethdb/kv_lmdb.go
@@ -652,7 +652,6 @@ func (tx *lmdbTx) Put(bucket string, k, v []byte) error {
 		if err != nil {
 			return err
 		}
-		defer c.Close()
 		return c.Put(k, v)
 	}
 
@@ -666,7 +665,6 @@ func (tx *lmdbTx) Delete(bucket string, k, v []byte) error {
 		if err != nil {
 			return err
 		}
-		defer c.Close()
 		return c.Delete(k, v)
 	}
 	err := tx.tx.Del(lmdb.DBI(b.DBI), k, v)
