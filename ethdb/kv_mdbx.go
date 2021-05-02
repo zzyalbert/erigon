cad450852337e1ca8cf68b5c4286c8f24d38bd16
alex.sharov <AskAlexSharov@gmail.com>
2021-05-02 15:43:46 +0700

    save

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -39,7 +39,7 @@ func NewMDBX() MdbxOpts {
 	return MdbxOpts{
 		bucketsCfg:        DefaultBucketConfigs,
 		flags:             mdbx.NoReadahead | mdbx.Coalesce | mdbx.Durable, // | mdbx.LifoReclaim,
-		dirtyListMaxPages: 64 * 1024,
+		dirtyListMaxPages: 32 * 1024,
 	}
 }
 
