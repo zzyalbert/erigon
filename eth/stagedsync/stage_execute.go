25f7fea78123b2bf34ada008d586ef46f7c784e3
alex.sharov <AskAlexSharov@gmail.com>
2021-06-02 10:38:12 +0700

    save

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -355,8 +355,8 @@ Loop:
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("al: %d,%d,%t\n", txDirty/1024/1024, txLimit/1024/1024, txDirty*2 > txLimit)
 			updateProgress := txDirty*2 > txLimit && !useExternalTx
+			fmt.Printf("al: %d,%d,%t, %t,%t\n", txDirty/1024/1024, txLimit/1024/1024, txDirty*2 > txLimit, useExternalTx, updateProgress)
 			if updateProgress {
 				if err = s.Update(tx, stageProgress); err != nil {
 					return err
