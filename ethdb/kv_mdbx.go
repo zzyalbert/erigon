4cb6f43c0fd082edfc6080ef35ed48eb0b467cc3
alex.sharov <AskAlexSharov@gmail.com>
2021-06-03 20:39:32 +0700

    less wrappers

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -969,7 +969,6 @@ func (tx *MdbxTx) stdCursor(bucket string) (RwCursor, error) {
 	b := tx.db.buckets[bucket]
 	c := &MdbxCursor{bucketName: bucket, tx: tx, bucketCfg: b, dbi: mdbx.DBI(tx.db.buckets[bucket].DBI), id: tx.cursorID}
 	tx.cursorID++
-	fmt.Printf("open:%s, %d,  %s\n", bucket, tx.cursorID, debug.Callers(9))
 	var err error
 	c.c, err = tx.tx.OpenCursor(c.dbi)
 	if err != nil {
