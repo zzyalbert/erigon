3296a9b25e272cb3d5af4a6b99438f6eae34543a
alex.sharov <AskAlexSharov@gmail.com>
2021-07-03 20:55:38 +0700

    less objects

--- core/state/history.go
+++ core/state/history.go
@@ -14,7 +14,11 @@ import (
 	"github.com/ledgerwatch/erigon/ethdb/bitmapdb"
 )
 
+var a = 0
+
 func GetAsOf(tx ethdb.Tx, storage bool, key []byte, timestamp uint64) ([]byte, error) {
+	a++
+	fmt.Printf("a: %d\n")
 	v, err := FindByHistory(tx, storage, key, timestamp)
 	if err == nil {
 		return v, nil
